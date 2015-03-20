b77889035dd483ddc978b56a655bdc8e2ba342db
Zarine <nicommki@hotmail.fr>
2015-03-20 03:27:10 +0100

    Only activate the mod during an actual game

--- DifficultyOptions/UI.cs
+++ DifficultyOptions/UI.cs
@@ -19,34 +19,39 @@ namespace DifficultyOptions
 
         public override void OnLevelLoaded(LoadMode mode)
         {
-            DebugOutputPanel.AddMessage(ColossalFramework.Plugins.PluginManager.MessageType.Message, "Zarine Difficulty Options Mod - Loading start");
-            // Add a new button to the view.
-            var uiView = UIView.GetAView();
-            _button = (zarineButton)uiView.AddUIComponent(typeof(zarineButton));
-            _button.configure("Difficulty");
-
-            _panel = (modPanel)uiView.AddUIComponent(typeof(modPanel));
-            _panel.setPanel("Difficulty Settings");
-
-            // Place the button.
-            _button.transformPosition = new Vector3(uiView.GetBounds().max.x - 0.3f, 0.105f - uiView.GetBounds().max.y);
-
-            // Respond to button click.
-            _button.eventClick += _panel.togglePanel;
-
-            /*var UIPanels = uiView.GetComponentsInChildren<UIPanel>();
-            for (var i = 0; i < UIPanels.Length; i++ )
+            if (mode == LoadMode.NewGame || mode == LoadMode.LoadGame)
             {
-                DebugOutputPanel.AddMessage(ColossalFramework.Plugins.PluginManager.MessageType.Message, "UIPanels: " + UIPanels[i].name);       
-            }*/
+                DebugOutputPanel.AddMessage(ColossalFramework.Plugins.PluginManager.MessageType.Message, "Zarine Difficulty Options Mod - Loading start");
+                // Add a new button to the view.
+                var uiView = UIView.GetAView();
+                _button = (zarineButton)uiView.AddUIComponent(typeof(zarineButton));
+                _button.configure("Difficulty");
 
-            DebugOutputPanel.AddMessage(ColossalFramework.Plugins.PluginManager.MessageType.Message, "Zarine Difficulty Options Mod - Loading end");
+                _panel = (modPanel)uiView.AddUIComponent(typeof(modPanel));
+                _panel.setPanel("Difficulty Settings");
+
+                // Place the button.
+                _button.transformPosition = new Vector3(uiView.GetBounds().max.x - 0.3f, 0.105f - uiView.GetBounds().max.y);
+
+                // Respond to button click.
+                _button.eventClick += _panel.togglePanel;
+
+                /*var UIPanels = uiView.GetComponentsInChildren<UIPanel>();
+                for (var i = 0; i < UIPanels.Length; i++ )
+                {
+                    DebugOutputPanel.AddMessage(ColossalFramework.Plugins.PluginManager.MessageType.Message, "UIPanels: " + UIPanels[i].name);       
+                }*/
+
+                DebugOutputPanel.AddMessage(ColossalFramework.Plugins.PluginManager.MessageType.Message, "Zarine Difficulty Options Mod - Loading end");
+            }
+            base.OnLevelLoaded(mode);
         }
 
         public override void OnLevelUnloading()
         {
             Object.Destroy(_button);
             Object.Destroy(_panel);
+            base.OnLevelUnloading();
         }
     }
 }
